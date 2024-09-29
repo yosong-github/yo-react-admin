c40b7f8fbfe8b4694c1acb8a7be24f3e437a5559
yosong <2404559603@qq.com>
2024-09-29 12:26:09 +0800

    chore: init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: yosong 2404559603@qq.com
  * @Date: 2024-09-29 11:25:56
  * @LastEditors: yosong 2404559603@qq.com
- * @LastEditTime: 2024-09-29 12:16:00
+ * @LastEditTime: 2024-09-29 12:21:32
  * @FilePath: \src\App.tsx
  */
 import { useState } from 'react'
